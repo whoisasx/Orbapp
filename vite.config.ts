409223e87b65809d3f9ed5cbc634913349c327f8
adil <adil.business4064@gmail.com>
2025-11-02 22:14:30 +0530

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,19 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), nodePolyfills(), tailwindcss()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+		target: "esnext",
+		minify: "esbuild",
+	},
+	optimizeDeps: {
+		include: ["react", "react-dom"],
+	},
+	define: {
+		global: "globalThis",
+	},
 });
